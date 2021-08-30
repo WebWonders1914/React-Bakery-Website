9c551f5a0fc2e763f542df10eaa52327af624567
WebWonders1914 <89780614+WebWonders1914@users.noreply.github.com>
2021-08-30 20:12:36 +0530

    Add files via upload

---
+++ src/components/pages/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MainPartProducts from '../MainPartProducts';
+import '../../App.css';
+
+export default function Products() {
+  return ( <>
+  <MainPartProducts />
+  
+  
+  </>) ;
+}
---
+++ src/components/pages/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../../App.css';
+
+export default function Services() {
+  return ( <>
+  <h1>We are at Your Service!</h1>
+  
+  
+  </>) ;
+}
